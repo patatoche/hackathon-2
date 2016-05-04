7cd48bf67fe054588e891ced110bae10f46fee5b
patatoche <prenaud@profideo.com>
2016-05-04 15:25:35 +0200

    :p

--- LevelB/Difference.php
+++ LevelB/Difference.php
@@ -33,17 +33,24 @@ class Difference
      */
     public function whatIsTheCostPlease()
     {
-        // @ TODO
-        $lenA = strlen($this->a);
-        $lenB = strlen($this->b);
+        $n = strlen($this->a);
+        $m = strlen($this->b);
 
-        for ($i = 1; $i <= $lenA; ++$i) {
-            for ($j = 1; $j <= $lenB; ++$j) {
-                $c = ($this->a[$i - 1] === $this->b[$j - 1]) ? 0 : 1;
-                $matrix[$i][$j] = 0;
+        for ($i = -1; $i < $n; $i++) {
+            $matrix[$i] = [];
+            $matrix[$i][-1] = $i + 1;
+        }
+        for ($j = -1; $j < $m; $j++) {
+            $matrix[-1][$j] = $j + 1;
+        }
+        for ($i = 0; $i < $n; $i++) {
+            for ($j = 0; $j < $m; $j++) {
+                $cout = $this->a[$i] == $this->b[$j] ? 0 : 1;
+                $matrix[$i][$j] = min(1 + $matrix[$i][$j - 1], 1 + $matrix[$i - 1][$j], $cout + $matrix[$i - 1][$j - 1]);
             }
         }
 
-        return $matrix[$lenA][$lenB];
+        return $matrix[$n - 1][$m - 1];
     }
+
 }
